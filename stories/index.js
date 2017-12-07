717073433afeee5a2f345ad7a9305325ffd26ba6
Travis Nesland <tnesland@gmail.com>
2017-12-07 13:55:46 -0500

    Color according to intensity

--- stories/index.js
+++ stories/index.js
@@ -1,18 +1,46 @@
 import React from 'react';
+import { times } from 'ramda'
 
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 
-/* import { Button, Welcome } from '@storybook/react/demo';
- *
- * storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
- *
- * storiesOf('Button', module)
- *   .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
- *   .add('with some emoji', () => <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>);*/
-
 import { ReactivityBox } from '../lib'
 
 storiesOf('ReactivityBox', module)
-  .add('foo', () => <ReactivityBox baseColor="#999" tileColor="green" />)
+  .add('green', () => {
+    const dataFetcher = () => new Promise((resolve, reject) =>
+      setTimeout(resolve(times(Math.random, 7 * 40)), 2400))
+
+    return (
+      <ReactivityBox
+        baseColor="#333"
+        fetchData={dataFetcher}
+        tileColor="green"
+      />
+    )
+  })
+  .add('yellow', () => {
+    const dataFetcher = () => new Promise((resolve, reject) =>
+      setTimeout(resolve(times(Math.random, 7 * 40)), 2400))
+
+    return (
+      <ReactivityBox
+        baseColor="#333"
+        fetchData={dataFetcher}
+        tileColor="yellow"
+      />
+    )
+  })
+  .add('red', () => {
+    const dataFetcher = () => new Promise((resolve, reject) =>
+      setTimeout(resolve(times(Math.random, 7 * 40)), 2400))
+
+    return (
+      <ReactivityBox
+        baseColor="#333"
+        fetchData={dataFetcher}
+        tileColor="red"
+      />
+    )
+  })
